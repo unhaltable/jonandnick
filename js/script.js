6577da2034b41fa4569b0d18dcfe7751df751ed8
Eugene Cheung <ey.cheung@outlook.com>
2014-03-18 16:37:26 -0400

    IT'S THE FINAL COUNTDOWN

--- js/script.js
+++ js/script.js
@@ -1,27 +1,6 @@
 $(document).ready(function(){
-  var stickyNavTop, scrollTop;
-
-  var stickyNav = function(){
-    stickyNavTop = window.innerHeight;
-    scrollTop = $(window).scrollTop();
-
-    $("header").css("opacity", 1 - scrollTop / stickyNavTop);
-
-    if (scrollTop > stickyNavTop) {
-      $('#nav').addClass('sticky');
-      $('#about').addClass('contentpush');
-    } else {
-      $('#nav').removeClass('sticky');
-      $('#about').removeClass('contentpush');
-    }
-  };
-
-  stickyNav();
-  $(window).scroll(function(){ stickyNav(); });
-  window.onresize = function(){ stickyNav(); }
-
   // Set the date we're counting down to.
-  var target_date = new Date("March 31, 2014").getTime();
+  var target_date = new Date("March 24, 2014").getTime();
 
   // Variables for time units.
   var days, hours, minutes, seconds;
@@ -66,23 +45,4 @@ $(document).ready(function(){
     while (s.length < size) s = "0" + s;
     return s;
   }
-
-  // Smooth scrollTo
-  var scrollTo = function(el, off){
-    $('html, body').animate({
-      scrollTop: $(el).offset().top - off
-    }, 650);
-  }
-
-  $("header").click(function(){ scrollTo("nav", 0); });
-  $("#more").click(function(){ scrollTo("nav", 0); });
-  $("#title").click(function(){ scrollTo("body", 0); });
-  $("#navabout").click(function(){
-    if (scrollTop <= stickyNavTop) scrollTo("nav", 0);
-    else scrollTo("#about", 0);
-  });
-  $("#navgoals").click(function(){ scrollTo("#goals", 50); });
-  $("#navvote").click(function(){ scrollTo("#vote", 50); });
-  $("#navcontact").click(function(){ scrollTo("#contact", 50); });
-  $("#navright").click(function(){ scrollTo("#vote", 50); });
 });
