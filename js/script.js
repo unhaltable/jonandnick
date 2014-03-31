00364352b6298128c0a3ac01468d73d8e4aecce2
Eugene Cheung <ey.cheung@outlook.com>
2014-03-30 22:32:57 -0400

    Dat map.

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
   function init_map() {
     var homeLatLng = new google.maps.LatLng(43.65950128716179,-79.39750556759793);
     var myOptions = {
-      zoom:17,
+      zoom:18,
       center: homeLatLng,
       mapTypeId: google.maps.MapTypeId.ROADMAP};
       map = new google.maps.Map(document.getElementById("gmap_canvas"), myOptions);
@@ -11,6 +11,7 @@ $(document).ready(function(){
         map: map,
         position: homeLatLng
       });
+      infowindow = new google.maps.InfoWindow({content:"<span style='height:auto !important; display:block; white-space:nowrap; overflow:hidden !important;'><strong style='font-weight:400;'>CSSU Office (BA 2283)</strong><br>40 St George Street<br> Toronto</span>" });
       google.maps.event.addListener(marker, "click", function(){ infowindow.open(map, marker); });
       google.maps.event.addDomListener(window, "resize", function(){ map.setCenter(homeLatLng); });
       infowindow.open(map,marker);
