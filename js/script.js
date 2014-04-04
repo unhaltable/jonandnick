22e1454be57e6c7273303426671797c094f5f63d
Eugene Cheung <ey.cheung@outlook.com>
2014-04-03 21:38:24 -0400

    Fixed JS.

--- js/script.js
+++ js/script.js
@@ -21,51 +21,51 @@ $(document).ready(function(){
   window.onresize = function(){ stickyNav(); }
 
   // Set the date we're counting down to.
-  var target_date = new Date("March 31, 2014").getTime();
+  // var target_date = new Date("March 31, 2014").getTime();
 
-  // Variables for time units.
-  var days, hours, minutes, seconds;
+  // // Variables for time units.
+  // var days, hours, minutes, seconds;
 
-  // Get the elements that will hold the numbers.
-  var $days = document.getElementById("d");
-  var $hours = document.getElementById("h");
-  var $minutes = document.getElementById("m");
-  var $seconds = document.getElementById("s");
+  // // Get the elements that will hold the numbers.
+  // var $days = document.getElementById("d");
+  // var $hours = document.getElementById("h");
+  // var $minutes = document.getElementById("m");
+  // var $seconds = document.getElementById("s");
 
-  // Calculate the countdown clock and set the HTML.
-  function update() {
-    // Find the amount of "seconds" between now and target.
-    var current_date = new Date().getTime();
-    var seconds_left = (target_date - current_date) / 1000;
+  // // Calculate the countdown clock and set the HTML.
+  // function update() {
+  //   // Find the amount of "seconds" between now and target.
+  //   var current_date = new Date().getTime();
+  //   var seconds_left = (target_date - current_date) / 1000;
 
-    // Do some time calculations.
-    days = parseInt(seconds_left / 86400);
-    seconds_left = seconds_left % 86400;
+  //   // Do some time calculations.
+  //   days = parseInt(seconds_left / 86400);
+  //   seconds_left = seconds_left % 86400;
 
-    hours = parseInt(seconds_left / 3600);
-    seconds_left = seconds_left % 3600;
+  //   hours = parseInt(seconds_left / 3600);
+  //   seconds_left = seconds_left % 3600;
 
-    minutes = parseInt(seconds_left / 60);
-    seconds = parseInt(seconds_left % 60);
+  //   minutes = parseInt(seconds_left / 60);
+  //   seconds = parseInt(seconds_left % 60);
 
-    // Format the number strings and put them in the elements.
-    $days.innerHTML = pad(days, 2);
-    $hours.innerHTML = pad(hours, 2);
-    $minutes.innerHTML = pad(minutes, 2);
-    $seconds.innerHTML = pad(seconds, 2);
-  }
+  //   // Format the number strings and put them in the elements.
+  //   $days.innerHTML = pad(days, 2);
+  //   $hours.innerHTML = pad(hours, 2);
+  //   $minutes.innerHTML = pad(minutes, 2);
+  //   $seconds.innerHTML = pad(seconds, 2);
+  // }
 
-  update();
-  // Update our number elements every 1 second
-  setInterval(update, 1000); // 1000 milliseconds = 1 second
+  // update();
+  // // Update our number elements every 1 second
+  // setInterval(update, 1000); // 1000 milliseconds = 1 second
 
   // This looks much better with leading zeros, don't you agree?
   // If num has less than size digits, add enough 0s to the front.
-  function pad(num, size) {
-    var s = num + "";
-    while (s.length < size) s = "0" + s;
-    return s;
-  }
+  // function pad(num, size) {
+  //   var s = num + "";
+  //   while (s.length < size) s = "0" + s;
+  //   return s;
+  // }
 
   // Smooth scrollTo
   var scrollTo = function(el, off){
